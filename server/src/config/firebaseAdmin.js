f52f15e0544450f2a42593fa23699d7e5b81b739
Michael Adeyemi <miknelandex@gmail.com>
2026-07-20 11:59:25 +0100

    Fix firebase admin for Render environment

--- server/src/config/firebaseAdmin.js
+++ server/src/config/firebaseAdmin.js
@@ -1,6 +1,6 @@
 import { cert, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import fs from "fs";
+
 
 const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT
